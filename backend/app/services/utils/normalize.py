5f030875cec5f5d4b2421895604350d2a679f6e9
Joseph Meza <joseph.meza@unmsm.edu.pe>
2026-05-12 16:42:09 -0500

    delete master merge

--- backend/app/services/utils/normalize.py
+++ backend/app/services/utils/normalize.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
 def normalize_for_db(df: pd.DataFrame) -> pd.DataFrame:
+
+    # datetime -> object
     for col in df.select_dtypes(include=["datetime", "datetimetz"]).columns:
         df[col] = df[col].astype(object)
-    return df.where(pd.notnull(df), None)
+
+    # IMPORTANTE:
+    # convertir todo a object para permitir None reales
+    df = df.astype(object)
+
+    # reemplazar NaN por None
+    df = df.where(pd.notnull(df), None)
+
+    return df
